643f057469a066af1c59e06f4c6f9cba47f5d18a
FazekasDomi <112930880+FazekasDomi@users.noreply.github.com>
2022-10-11 09:40:17 +0200

    Add files via upload

---
+++ 11_12_ora.py
@@ -0,0 +1,25 @@
+'''gondolt_sam = 4
+tipp = input ('gondoltam egy számra')
+tipp = int (tipp)
+ if tipp == gondolt_sam:
+     print ('ügyes')
+     print ('ez az igaz ág')
+ else:
+     print('hosszan gondolkodtél?')
+     print('nem érte meg')
+ print ('pápá')'''
+
+
+ szam1= input ('kérek egy szamot')
+ szam1= int (szam1)
+ szam2= input ('mégeggyet')
+ szam2= int (szam2)
+ if szam1==szam2:
+     print('két szám megeggyezik')
+ if szam1!= szam2:
+     print ('két szám nem eggyezik')
+ print('program vége')
+ if szam1 <=szam2: 
+     print ('számod kissebb')
+ else:
+     print ('nagyobb vagy eggyenlö')
