451878a59fb64d20215a88d1e29b5adfb42d7eb8
FazekasDomi <112930880+FazekasDomi@users.noreply.github.com>
2022-11-15 08:05:42 +0100

    Add files via upload

---
+++ 1516_ora.py
@@ -0,0 +1,95 @@
+'''print("Hello")
+
+print('Hello')'''
+
+# ----------------------------------------------------------
+
+'''
+a = "Hello"
+
+print(a)
+'''
+# ----------------------------------------------------------
+'''
+a = """Lorem ipsum dolor sit amet,
+
+consectetur adipiscing elit,
+
+sed do eiusmod tempor incididunt
+
+ut labore et dolore magna aliqua."""
+
+print(a)
+
+
+a = Lorem ipsum dolor sit amet,
+
+consectetur adipiscing elit,
+
+sed do eiusmod tempor incididunt
+
+ut labore et dolore magna aliqua.
+
+print(a)'''
+
+# ----------------------------------------------------------
+'''
+a = "Hello, World!"
+
+print(a[1])
+
+print(a[4])'''
+#------------------------
+'''
+ print(x)
+
+for x in "banana":
+
+  print(x,end=' ')'''
+#------------------------
+'''
+a = "Hello, World!"
+samlalo = 1
+for x in a:
+    if samlalo % 2==0
+     print (a)'''
+
+#-------------------------------------------------
+'''
+txt = "The best things in life are free!"
+
+print("free" in txt) '''
+#------------------------------------------
+'''
+b = "Hello, World!"
+
+print(b[2:5])  '''
+#------------------------------------------
+'''
+b = "Hello, World!"
+
+print(b[:5]) 
+'''
+# ----------------------------------------------------------
+'''
+b = "Hello, World!"
+
+print(b[2:]) 
+'''
+# ----------------------------------------------------------
+'''
+b = "Hello, World!"
+
+print(b[-5:-2]) # orl
+'''
+# ----------------------------------------------------------
+'''
+a = "Hello, World!"
+
+print(a.replace("J", "H"))
+'''
+# ----------------------------------------------------------
+'''a = "Hello, Jorld!"
+
+print(a.replace("J", "W"))'''
+# ----------------------------------------------------------
