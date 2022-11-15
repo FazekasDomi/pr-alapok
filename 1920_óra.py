ce02685a5b659aefcb5d2ce9dd19c841455aaf98
FazekasDomi <112930880+FazekasDomi@users.noreply.github.com>
2022-11-15 09:36:02 +0100

    Add files via upload

---
+++ 1920_óra.py
@@ -0,0 +1,32 @@
+'''
+print("ez\"egy\"sztring")
+'''
+
+'''
+ szam = 0
+  while szam != 100:
+    szam = szam + 1      
+    print(szam)
+'''
+'''
+ szam = 1
+  while szam != 100:
+    print(szam)
+    szam = szam + 1 
+'''  
+'''   
+ szam = 1
+  while szam != 101:
+    print(szam)
+    szam = szam + 1 
+'''
+'''
+szam = 1
+while True:
+    print(szam)
+    szam = szam + 1 '''
+'''
+szam = 1
+while szam <= 101:
+    print(szam)
+    szam = szam + 1 '''
---
+++ házi.py
@@ -0,0 +1,22 @@
+'''
+szam = 1
+while szam <= 101:
+    print(szam)
+    szam = szam + 2 '''
+'''
+szam = 1
+while szam <= 101:
+    print(szam)
+    szam = szam + 3 '''
+
+'''
+szam = 1
+szam = szam + 1
+while szam <= 101:
+    print(szam)
+    szam = szam + 2 '''
+'''
+szam = 1
+while szam <= 101:
+    print(szam)
+    szam = szam + 2 '''
