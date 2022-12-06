68f2364d243bc65f4aec59993149a73af39d3fdb
FazekasDomi <112930880+FazekasDomi@users.noreply.github.com>
2022-12-06 08:21:46 +0100

    Add files via upload

---
+++ 2324.py
@@ -0,0 +1,13 @@
+'''
+szoveg = "hello világa!" 
+for karakter in szoveg:
+
+    print(karakter,end="")
+'''
+'''
+for szam in range (50,101,3):
+    print(szam)'''
+
+name = input ("add meg a neved! ")
+for karakter in name:
+    print(karakter,end="")
---
+++ 9_10_óra.py
@@ -0,0 +1,18 @@
+# Első feladat 9dk tankönyv
+
+IDEI_ÉV = 2022
+print(type(IDEI_ÉV))
+print( 'Az idei év :',IDEI_ÉV, sep='--->')
+felhasználó_kora = input('hány éves vagy?')
+print(type(felhasználó_kora))
+print('Te most', felhasználó_kora,'éves vagy.')
+felhasználó_kora = int(felhasználó_kora)
+születési_év = IDEI_ÉV - felhasználó_kora
+print('Ekkor születtél:', születési_év,'.', sep='')
+
+#második program
+gondolt szám=4
+tip=input('melyik számra gondoltam?')
+ if condition:gondolt szám = tip 
+     pass print ('igen')
+ 
