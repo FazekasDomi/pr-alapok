fb50d457d39f16001ea5c7bd7b2b8b2f11d41709
FazekasDomi <112930880+FazekasDomi@users.noreply.github.com>
2023-01-09 14:30:48 +0100

    Add files via upload

---
+++ házi20230110.py
@@ -0,0 +1,52 @@
+
+#elágazások
+#1.1
+'''
+szam1 =  input ( 'jó napod volt?' )
+szam2=("nem")
+szam3=("igen")
+if szam1 == szam2 :
+    print("sajnálom")
+if szam1 == szam3:
+    print("az jó")'''
+    #1.2
+'''
+szam1 =  input ( 'jó napod volt?' )
+szam2=("nem")
+szam3=("igen")
+if szam1 == szam2 :
+    print("sajnálom")
+if szam1 == szam3:
+    print("az jó")
+else :
+    print("nem értem")'''
+    #3
+'''
+tipp = input ('gondoltam egy számra')
+tipp = int (tipp)
+if tipp == gondolt_sam:
+     nyomtat ('ügyes')
+     print ('ez az igaz ág')
+más:
+     print('hosszan gondolkodtél?')
+     print('nem érte meg')
+nyomtat ('pápá')'''
+#while ciklus
+#1
+'''
+n = 0
+while n < 10:
+    n =n+2
+print (n)'''
+#2
+'''
+n = 10
+while n > 0:
+    n =n-2
+print (n)'''
+#3
+'''
+n = 1
+while n < 10:
+    n =n+2
+print (n)'''
